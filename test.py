6a8c0ef05e7886e22dac455952f48f4425489a68
piko567 <piko567@tamu.edu>
2018-02-18 21:29:31 -0600

    testing with only one subreddit

---
+++ test.py
@@ -0,0 +1,50 @@
+import praw
+
+reddit = praw.Reddit(client_id='yvtEWUUqvd0tYQ',
+                     client_secret='JVrzr8bzhejekYdCkyqhg9SijTU',
+                     user_agent='nobusohaju')
+
+from praw.models import MoreComments
+
+
+#f = open('subredditlist.txt', 'r')
+
+#for line in f
+
+subreddit = reddit.subreddit('leagueoflegends')
+
+x = 0
+
+#run a loop through the top 10 posts
+for submission in subreddit.top(limit=10):
+
+	num = str(x) 
+	name = 'leagueoflegends' + '-' + num + '.txt'
+
+	out = open(name, 'w')
+	out.write(' ')
+
+	#grab the title of the post
+	#grab the ID of the post
+	postTitle = submission.title
+	postID = submission.id
+
+	#this should work, but iterate through the comment tree grabbing each comment
+	#next two lines are how we clean up the comment tree and allow for iterative search of comments
+	submission.comments.replace_more(limit=None)
+
+	#run through comments
+	for comment in submission.comments.list():
+		
+		#save the comment to a text so we can output it to wherever we decide
+		text = comment.body
+
+		#we now have the body just need to save it somewhere
+		out.write(text)
+		out.write(' ')
+	x += 1
+
+
+#should be end of program
+
+
